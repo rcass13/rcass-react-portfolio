f46b3752086534aa7fb387da1972cefca3de02df
RoseCass <rosecassidy1303@gmail.com>
2023-06-15 12:22:35 -0700

    targets added to links

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import weatherImage from '../../assets/images/weather_app.jpg';
-import exprecipesImage from '../../assets/images/expRecipes.jpg';
-import astrologicImage from '../../assets/images/astroLOGIC.jpg';
-import Explorot from "../../assets/images/Explorot.jpg"
-import sparklePlaceholder from '../../assets/images/sparklePlaceholder.png'
-
+import React from "react";
+import weatherImage from "../../assets/images/weather_app.jpg";
+import exprecipesImage from "../../assets/images/expRecipes.jpg";
+import astrologicImage from "../../assets/images/astroLOGIC.jpg";
+import Explorot from "../../assets/images/Explorot.jpg";
+import sparklePlaceholder from "../../assets/images/sparklePlaceholder.png";
 
 const Card = ({ imageSrc, title, description, link, color }) => {
   return (
@@ -14,7 +13,14 @@ const Card = ({ imageSrc, title, description, link, color }) => {
         <div className="card-body">
           <h5 className="card-title text-white">{title}</h5>
           <p className="card-text text-white">{description}</p>
-          <a href={link} className="btn btn-outline-light">View Project</a>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-outline-light"
+          >
+            View Project
+          </a>{" "}
         </div>
       </div>
     </div>
@@ -23,10 +29,17 @@ const Card = ({ imageSrc, title, description, link, color }) => {
 
 export default function Portfolio() {
   return (
-    <div style={{ margin:"70px",width:'max-width', height:'fit-content', paddingBottom:"100px"}}>
+    <div
+      style={{
+        margin: "70px",
+        width: "max-width",
+        height: "fit-content",
+        paddingBottom: "100px",
+      }}
+    >
       <h1>SOME OF MY PROJECTS:</h1>
       <div className="row">
-        <Card 
+        <Card
           imageSrc={weatherImage}
           title="Project 1: Weather App"
           description="FIVE DAY WEATHER FORECAST WHEN YOU SEARCH ANY CITY!"
@@ -34,15 +47,14 @@ export default function Portfolio() {
           target="_blank"
           color="primary"
         />
-        <Card 
+        <Card
           imageSrc={exprecipesImage}
           title="Project 2: ExpRecipes"
           description="SEARCH FOR RECIPES AND ADD INGREDIENTS TO YOUR GROCERY LIST!"
           link="https://joshuaelitate.github.io/ExpRecipe/"
-          target="_blank"
           color="danger"
         />
-        <Card 
+        <Card
           imageSrc={astrologicImage}
           title="Project 3: AstroLOGIC"
           description="MAKE AN ACCOUNT TO VIEW ASTROLOGICAL INFORMATION AND HOROSCOPE!"
@@ -50,7 +62,7 @@ export default function Portfolio() {
           target="_blank"
           color="warning"
         />
-        <Card 
+        <Card
           imageSrc={Explorot}
           title="Explorot"
           description="LEARN THE TAROT WITH NUMEROLOGY, ASTROLOGY, AND MEANINGS!"
@@ -58,7 +70,7 @@ export default function Portfolio() {
           target="_blank"
           color="success"
         />
-        <Card 
+        <Card
           imageSrc={sparklePlaceholder}
           title="Project 5 Coming Soon!"
           description="Working on beefing up the portfolio! Check back again soon <3"
@@ -66,7 +78,7 @@ export default function Portfolio() {
           target="_blank"
           color="info"
         />
-        <Card 
+        <Card
           imageSrc={sparklePlaceholder}
           title="Project 6 Coming Soon!"
           description="Working on beefing up the portfolio! Check back again soon <3"
@@ -77,4 +89,4 @@ export default function Portfolio() {
       </div>
     </div>
   );
-};
+}
